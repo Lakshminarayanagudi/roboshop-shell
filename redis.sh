c46abd074e5c14ef25a1a849926d318c68f26e91
lakshminarayanagudi <lakshminarayanagudi1@gmail.com>
2024-06-19 10:23:19 +0530

    learn-github

--- redis.sh
+++ redis.sh
@@ -35,6 +35,7 @@ dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
 VALIDATE $? "Installing Remi release"
 
 dnf module enable redis:remi-6.2 -y
+
 VALIDATE $? "enabling redis"
 
 dnf install redis -y
