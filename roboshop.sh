33d36c581e11f66eab9c3adc496474055a327d0c
lakshminarayanagudi <lakshminarayanagudi1@gmail.com>
2024-08-02 22:51:16 +0530

    learn-github

--- roboshop.sh
+++ roboshop.sh
@@ -3,6 +3,8 @@
 AMI=ami-0b4f379183e5706b9
 SG_ID=sg-0d75384e0bdfd9e73 #replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+ZONE_ID=Z0899623A53UDAB4K9OV
+DOMAIN_NAME="daws45.online"
 
 for i in "${INSTANCES[@]}"
 do
@@ -14,5 +16,25 @@ do
     fi
 
 IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-0d75384e0bdfd9e73 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
-echo "$i: $IP_ADDRESS"
+   echo "$i: $IP_ADDRESS"
+   
+   #create R53 record, make sure you delete existing record
+   aws route53 change-resource-record-sets \
+   --hosted-zone-id $ZONE_ID \
+   --change-batch "
+   {
+      "Comment": "Testing creating a record set"
+      ,"Changes": [{
+      "Action"              : "CREATE"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "$i.$DOMAIN_NAME"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "$IP_ADDRESS"
+        }]
+      }
+      }]
+    }
+  "
 done
